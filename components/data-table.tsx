2d1ecae11731674b20b323ce437be1faeef2fbe4
bueno <joaopedrobueno2910@gmail.com>
2024-09-04 22:19:56 -0300

    implementando delete account
    implementando delete account

--- components/data-table.tsx
+++ components/data-table.tsx
@@ -25,6 +25,7 @@ import {
 } from "@/components/ui/table";
 import React from "react";
 import { Trash } from "lucide-react";
+import { useConfirm } from "@/hooks/use-confirm";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -41,6 +42,10 @@ export function DataTable<TData, TValue>({
   onDelete,
   disabled,
 }: DataTableProps<TData, TValue>) {
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Você tem certeza?",
+    "você está prestes a realizar uma exclusão."
+  );
   const [sorting, setSorting] = React.useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
     []
@@ -66,6 +71,7 @@ export function DataTable<TData, TValue>({
 
   return (
     <div>
+      <ConfirmDialog />
       <div className="flex items-center py-4">
         <Input
           placeholder={`Filtre por ${filterKey}...`}
@@ -81,6 +87,14 @@ export function DataTable<TData, TValue>({
             size="sm"
             variant="outline"
             className="ml-auto font-normal text-xs"
+            onClick={async () => {
+              const ok = await confirm();
+
+              if (ok) {
+                onDelete(table.getFilteredSelectedRowModel().rows);
+                table.resetRowSelection;
+              }
+            }}
           >
             <Trash className="size=4 mr-2" />
             Deletar ({table.getFilteredSelectedRowModel().rows.length})
